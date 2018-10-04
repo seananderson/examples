388a051ef02c15a5dc52fd4c863a788b4ce1cbfc
Sean Anderson <sean@seananderson.ca>
2018-10-04 14:16:53 -0700

    Enable eye control

--- eye_control.py
+++ eye_control.py
@@ -1,10 +1,10 @@
-# import eye_mouse
-# from talon.voice import Word, Context, Key, Rep, Str, press
-# 
-# ctx = Context('eye_control')
-# ctx.keymap({
-#     'debug overlay':   lambda m: eye_mouse.on_menu('Eye Tracking >> Show Debug Overlay'),
-#     'control mouse':   lambda m: eye_mouse.on_menu('Eye Tracking >> Control Mouse'),
-#     'camera overlay':  lambda m: eye_mouse.on_menu('Eye Tracking >> Show Camera Overlay'),
-#     'run calibration': lambda m: eye_mouse.on_menu('Eye Tracking >> Calibrate'),
-# })
+import eye_mouse
+from talon.voice import Word, Context, Key, Rep, Str, press
+
+ctx = Context('eye_control')
+ctx.keymap({
+    'toggle debug overlay':   lambda m: eye_mouse.on_menu('Eye Tracking >> Show Debug Overlay'),
+    'toggle Tobi':   lambda m: eye_mouse.on_menu('Eye Tracking >> Control Mouse'),
+    'toggle camera overlay':  lambda m: eye_mouse.on_menu('Eye Tracking >> Show Camera Overlay'),
+    'run calibration': lambda m: eye_mouse.on_menu('Eye Tracking >> Calibrate'),
+})
