34edcf9318cc0c99582f1c92af298b13f6307060
Sean Anderson <sean@seananderson.ca>
2018-10-04 14:17:14 -0700

    Add a Mail.app configuration

---
+++ mail.py
@@ -0,0 +1,16 @@
+from talon.voice import Context, Key
+
+ctx = Context('mail', bundle='com.apple.mail')
+
+keymap =  {
+    'message send please': Key('cmd-shift-d'),
+    'message viewer new': Key('cmd-shift-n'),
+    'message new': Key('cmd-n'),
+    'message reply': Key('cmd-r'),
+    'message reply all': Key('cmd-shift-r'),
+    'message forward': Key('cmd-shift-f'),
+    'message archive': Key('cmd-ctrl-a'),
+    'toggle filter': Key('cmd-l'),
+}
+
+ctx.keymap(keymap)
