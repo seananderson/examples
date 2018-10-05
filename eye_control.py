982c1e9d9e58f58a39b4591b003d94263fc48f68
Sean Anderson <sean@seananderson.ca>
2018-10-04 19:50:56 -0700

    Switch eye control commands

--- eye_control.py
+++ eye_control.py
@@ -3,8 +3,8 @@ from talon.voice import Word, Context, Key, Rep, Str, press
 
 ctx = Context('eye_control')
 ctx.keymap({
-    'toggle debug overlay':   lambda m: eye_mouse.on_menu('Eye Tracking >> Show Debug Overlay'),
-    'toggle Tobi':   lambda m: eye_mouse.on_menu('Eye Tracking >> Control Mouse'),
-    'toggle camera overlay':  lambda m: eye_mouse.on_menu('Eye Tracking >> Show Camera Overlay'),
+    '(toggle|run) debug overlay':   lambda m: eye_mouse.on_menu('Eye Tracking >> Show Debug Overlay'),
+    'Tobi':   lambda m: eye_mouse.on_menu('Eye Tracking >> Control Mouse'),
+    '(toggle|run)  camera overlay':  lambda m: eye_mouse.on_menu('Eye Tracking >> Show Camera Overlay'),
     'run calibration': lambda m: eye_mouse.on_menu('Eye Tracking >> Calibrate'),
 })
