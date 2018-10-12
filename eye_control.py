8ebaab18f6b3a0459e7df7c054cfcc0d7311e1e9
Ryan Hileman <lunixbochs@gmail.com>
2018-10-12 11:58:56 -0700

    update examples for 0.0.8

--- eye_control.py
+++ eye_control.py
@@ -1,10 +1,10 @@
-import eye_mouse
-from talon.voice import Word, Context, Key, Rep, Str, press
+from talon_plugins import eye_mouse
+from talon.voice import Context
 
 ctx = Context('eye_control')
 ctx.keymap({
-    '(toggle|run) Tobi debug':   lambda m: eye_mouse.on_menu('Eye Tracking >> Show Debug Overlay'),
-    'Tobi':   lambda m: eye_mouse.on_menu('Eye Tracking >> Control Mouse'),
-    '(toggle|run) Tobi overlay':  lambda m: eye_mouse.on_menu('Eye Tracking >> Show Camera Overlay'),
-    'run Tobi calibration': lambda m: eye_mouse.on_menu('Eye Tracking >> Calibrate'),
+    'debug overlay':   lambda m: eye_mouse.debug_overlay.toggle(),
+    'Tobi':   lambda m: eye_mouse.control_mouse.toggle(),
+    'camera overlay':  lambda m: eye_mouse.camera_overlay.toggle(),
+    'run calibration': lambda m: eye_mouse.calib_start(),
 })
