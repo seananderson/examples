8615b1c820da70e31a678ffa5a128ad149266b1e
Sean Anderson <sean@seananderson.ca>
2018-12-13 09:27:24 -0800

    Update log parsing script

--- log.R
+++ log.R
@@ -1,7 +1,16 @@
-d <- readr::read_csv("~/.talon/phrase_log.csv",
-  col_names = c("phrase", "date", "app"),
-  col_types = readr::cols(
-    phrase  = readr::col_character(),
-    date    = readr::col_datetime(format = ""),
-    app     = readr::col_character()
-))
+d <- read.csv("~/.talon/phrase_log.csv",
+  col.names = c("phrase", "date", "app"),
+  stringsAsFactors = FALSE, strip.white = TRUE)
+d$date <- lubridate::ymd_hms(d$date)
+
+library(dplyr)
+d %>%
+  group_by(phrase) %>%
+  summarize(n = n()) %>%
+  arrange(-n) %>%
+  top_n(10)
+
+d <- d %>% mutate(time_difference = c(NA, date[-1] - date[-length(date)])) %>%
+  mutate(short_difference = time_difference < 3) %>%
+  group_by(app)
+head(d)
